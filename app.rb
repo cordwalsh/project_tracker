8d48b2d912872595f78982d606f1614674dca4c6
Cord Walsh <cordwalsh@Cords-MacBook-Air.local>
2019-08-12 14:44:35 -0700

    adding functionality

--- app.rb
+++ app.rb
@@ -1,9 +1,21 @@
 require('sinatra')
 require('sinatra/reloader')
-require('./lib/volunteer')
 require('./lib/project')
 require('pry')
 also_reload('lib/**/*.rb')
 require("pg")
 
 DB = PG.connect({:dbname => "project_tracker"})
+
+get ('/') do
+  @projects = Project.all
+  erb(:index)
+end
+
+get ('/new_project') do
+  erb(:new_project)
+end
+
+get ('/projects') do
+  redirect to('/')
+end
--- lib/project.rb
+++ lib/project.rb
@@ -6,3 +6,30 @@ class Project
     @title = attributes.fetch(:title)
     @id = attributes.fetch(:id)
   end
+
+  def self.all
+    returned_projects = DB.exec("SELECT * FROM projects;")
+    projects = []
+    returned_projects.each() do |project|
+      title = project.fetch("title")
+      id = project.fetch("id").to_i
+      projects.push(Project.new({:title => title, :id => id}))
+    end
+    projects
+  end
+
+  def self.clear
+    DB.exec("DELETE FROM projects *;")
+  end
+
+  def ==(project_to_compare)
+    self.title() ==project_to_compare.title()
+  end
+
+  def save
+  result = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;")
+  @id = result.first().fetch("id").to_i
+  end
+
+
+end
